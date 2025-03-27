5e83827cc8316baddde98fa1195861610a768f34
Mark LauQ <mark.lauq@gmail.com>
2025-03-27 15:56:58 +0800

    fix: start script

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "standalone",
+  basePath: isProd ? "/next-router-demo" : undefined,
+  assetPrefix: isProd ? "/next-router-demo" : "",
+  trailingSlash: true,
 };
 
 export default nextConfig;
